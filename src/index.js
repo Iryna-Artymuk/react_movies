601ede5495256679e7b9e8a90453e820895a8c5b
Iryna Artymuk <artimyk@ukr.net>
2023-10-08 15:47:14 +0300

    add react pre render

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import isPropValid from '@emotion/is-prop-valid';
-
+import { hydrate, render } from 'react-dom';
 import { StyleSheetManager } from 'styled-components';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -10,15 +10,38 @@ import App from './App';
 //  підхід з перемиканням теми у style components.
 // створити об'єкти з однаковими ключами, але різними кольорами. Просто перемикаю ці два об'єкта у ThemeProvider
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  // <React.StrictMode>
-  <BrowserRouter basename="react_movies">
-    <StyleSheetManager shouldForwardProp={isPropValid}>
-      <App />
-    </StyleSheetManager>
-  </BrowserRouter>
-  // </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   // <React.StrictMode>
+//   <BrowserRouter basename="react_movies">
+//     <StyleSheetManager shouldForwardProp={isPropValid}>
+//       <App />
+//     </StyleSheetManager>
+//   </BrowserRouter>
+//   // </React.StrictMode>
+// );
 
-// basename="react_movies"
+// // basename="react_movies"
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+
+if (rootElement.hasChildNodes()) {
+hydrate(
+    <BrowserRouter basename="react_movies">
+      <StyleSheetManager shouldForwardProp={isPropValid}>
+        <App />
+      </StyleSheetManager>
+    </BrowserRouter>,
+    rootElement
+  );
+} else {
+ render(
+    <BrowserRouter basename="react_movies">
+      <StyleSheetManager shouldForwardProp={isPropValid}>
+        <App />
+      </StyleSheetManager>
+    </BrowserRouter>,
+    rootElement
+  );
+}
