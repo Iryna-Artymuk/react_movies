207edbbf224e49ceec7b6cf94f3decea2676bb7c
Iryna Artymuk <artimyk@ukr.net>
2023-10-08 16:29:30 +0300

    remove snap shot

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
- import { render } from 'react-snapshot';
+
 import isPropValid from '@emotion/is-prop-valid';
 
 import { StyleSheetManager } from 'styled-components';
@@ -8,28 +8,28 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 
-//  підхід з перемиканням теми у style components.
-// створити об'єкти з однаковими ключами, але різними кольорами. Просто перемикаю ці два об'єкта у ThemeProvider
+ //підхід з перемиканням теми у style components.
+//створити об'єкти з однаковими ключами, але різними кольорами. Просто перемикаю ці два об'єкта у ThemeProvider
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   // <React.StrictMode>
-//   <BrowserRouter basename="react_movies">
-//     <StyleSheetManager shouldForwardProp={isPropValid}>
-//       <App />
-//     </StyleSheetManager>
-//   </BrowserRouter>
-//   // </React.StrictMode>
-// );
-
-// // basename="react_movies"
-
-
-render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  // <React.StrictMode>
   <BrowserRouter basename="react_movies">
     <StyleSheetManager shouldForwardProp={isPropValid}>
       <App />
     </StyleSheetManager>
-  </BrowserRouter>,
-  document.getElementById('root')
+  </BrowserRouter>
+  // </React.StrictMode>
 );
+
+// // basename="react_movies"
+
+
+// render(
+//   <BrowserRouter basename="react_movies">
+//     <StyleSheetManager shouldForwardProp={isPropValid}>
+//       <App />
+//     </StyleSheetManager>
+//   </BrowserRouter>,
+//   document.getElementById('root')
+// );
